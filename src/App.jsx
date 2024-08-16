f464596c753da33799f51c1c0cf45836175de28a
JoanChesire <157241944+JoanChesire@users.noreply.github.com>
2024-08-16 07:17:02 +0000

    More Updates

--- src/App.jsx
+++ src/App.jsx
@@ -29,7 +29,7 @@ const siteProps = {
   email: "",
   gitHub: "JoanChesire",
   instagram: "",
-  linkedIn: "linkedin.com/in/joan-chesire-7b11aa130",
+  linkedIn: "Joan Chesire",
   medium: "",
   twitter: "@Chesirejoan",
   youTube: "",
