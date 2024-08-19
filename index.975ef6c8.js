3c85f54943fccf0d069281b07aa5c18f500d3f55
JoanChesire <157241944+JoanChesire@users.noreply.github.com>
2024-08-19 12:14:25 +0000

    Updates

--- index.975ef6c8.js
+++ index.975ef6c8.js
@@ -27092,7 +27092,7 @@ var _stylesCss = require("./styles.css");
     email: "",
     gitHub: "JoanChesire",
     instagram: "",
-    linkedIn: "@joan-chesire",
+    linkedIn: "joanchesire",
     medium: "",
     twitter: "@Chesirejoan",
     youTube: ""
